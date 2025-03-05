317a8aa41be77583c66e47bcd8058f073dae3ffa
leehendrick <imhendrick16@gmail.com>
2025-03-05 17:19:26 -0500

    chore: update user table migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,11 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('avatar')->default('default.png');
+            $table->enum('role', ['admin', 'user'])->default('user');
+            $table->text('bio')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
