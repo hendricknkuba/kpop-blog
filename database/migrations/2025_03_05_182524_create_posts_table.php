0c3f983c98ee23c6bed2abaf81fcfa59f40b8d86
leehendrick <imhendrick16@gmail.com>
2025-03-05 17:20:04 -0500

    feat(db): create post table migration

---
+++ database/migrations/2025_03_05_182524_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('author_id')   ;
+            $table->string('title');
+            $table->string('slug');
+            $table->text('content');
+            $table->string('image')->default('default.png');
+            $table->string('category');
+            $table->enum('status', ['published', 'draft', 'archived'])->default('draft');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
