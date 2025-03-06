81f410918a14b6a7176d11eeb501261dbedaa066
leehendrick <imhendrick16@gmail.com>
2025-03-06 01:30:19 -0500

    feat: implementing json api response schema

---
+++ app/Traits/ApiResponses.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Traits;
+
+trait ApiResponses
+{
+    protected function ok($message, $data = [], $statusCode = 200)
+    {
+        return response()->json([
+            'data' => $data,
+            'message' => $message,
+            'status' => $statusCode,
+        ], $statusCode);
+
+    }
+
+    protected function error($errors = [], $statusCode = 500)
+    {
+        if (is_string($errors)) {
+            return response()->json([
+                'message' => $errors,
+                'status' => $statusCode,
+            ], $statusCode);
+        }
+
+        return response()->json([
+           'errors' => $errors,
+        ]);
+    }
+
+    protected function notAuthorized($message)
+    {
+        return $this->error([
+            'status' => 401,
+            'message' => $message,
+            'source' => '',
+        ]);
+    }
+}
