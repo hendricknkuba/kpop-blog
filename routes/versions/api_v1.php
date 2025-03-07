f1ddb290648251ecb07785d3a174d32a6ca1140d
leehendrick <imhendrick16@gmail.com>
2025-03-07 01:12:08 -0500

    feat: add users routes

--- routes/versions/api_v1.php
+++ routes/versions/api_v1.php
@@ -1,8 +1,15 @@
 <?php
+
+use App\Http\Controllers\Api\v1\UsersController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::get('/health-check', function () {
         return response()->json(['status' => 'API v1 OK']);
     });
+});
+
+Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
+
+    Route::apiResource('users', UsersController::class)->except('update');
 });
