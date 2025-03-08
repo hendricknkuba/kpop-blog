7149e2e0e5e3476fe1b2bc019f4788698ba22dc7
leehendrick <imhendrick16@gmail.com>
2025-03-08 00:28:13 -0500

    feat: apply filter to query

---
+++ app/Http/Filters/v1/QueryFilter.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Filters\v1;
+
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
+
+abstract class QueryFilter
+{
+    protected $builder;
+    protected $request;
+    protected $sortable = [];
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function apply($builder): Builder
+    {
+        $this->builder = $builder;
+
+        foreach ($this->request->all() as $key => $value) {
+            if (method_exists($this, $key)) {
+                $this->$key($value);
+            }
+        }
+
+        return $this->builder;
+    }
+
+    protected function filter($arr): Builder
+    {
+        foreach ($arr as $key => $value) {
+            if (method_exists($this, $key)) {
+                $this->$key($value);
+            }
+        }
+
+        return $this->builder;
+    }
+
+    protected function sort($value): void
+    {
+        $sortAttributes = explode(',', $value);
+
+        foreach ($sortAttributes as $sortAttribute) {
+            $direction = 'asc';
+
+            if (strpos($sortAttribute, '-') === 0) {
+                $direction = 'desc';
+                $sortAttribute = substr($sortAttribute, 1);
+            }
+
+            if (in_array($sortAttribute, $this->sortable) && !array_key_exists($sortAttribute, $this->sortable)) {
+                continue;
+            }
+
+            $columnName = $this->sortable[$sortAttribute] ?? null;
+
+            if ($columnName === null) {
+                $columnName = $sortAttribute;
+            }
+
+            $this->builder->orderBy($columnName, $direction);
+        }
+    }
+}
