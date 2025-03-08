1438dc64fc66b8fc01e4568fdbb31fda01f634ba
leehendrick <imhendrick16@gmail.com>
2025-03-08 00:34:30 -0500

    refactor: changing the logic to add data when the route is post.show

--- app/Http/Resources/v1/PostResource.php
+++ app/Http/Resources/v1/PostResource.php
@@ -19,20 +19,20 @@ class PostResource extends JsonResource
                 'type' => 'posts',
                 'id' => $this->id,
                 'attributes' => array_merge([
-                    'author_id' => $this->author_id,
+                    'author_id' => $this->user_id,
                     'title' => $this->title,
                     'content' => $this->content,
                     'image' => $this->image,
-                    ], $this->mergeWhen($request->routeIs('posts.show'),
+                    ] + array_filter(
                     [
-                        'slug' => $this->slug,
-                        'status' => $this->status,
-                        'category' => $this->category,
-                        'createdAt' => $this->created_at,
-                        'updatedAt' => $this->updated_at,
+                        'slug' => $this->when($request->routeIs('posts.show'), $this->slug),
+                        'status' => $this->when($request->routeIs('posts.show'), $this->status),
+                        'category' => $this->when($request->routeIs('posts.show'), $this->category),
+                        'createdAt' => $this->when($request->routeIs('posts.show'), $this->created_at),
+                        'updatedAt' => $this->when($request->routeIs('posts.show'), $this->updated_at),
                     ])),
                 'links' => [
-                    'self' => route('posts.show', ['posts' => $this->id])
+                    'self' => route('posts.show', ['post' => $this->id])
                 ]
             ]
         ];
