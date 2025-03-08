6a4d7c246e4750f4d697c01f614cd6a033ad562c
leehendrick <imhendrick16@gmail.com>
2025-03-08 00:29:55 -0500

    feat: search all users and search specific user by id

--- app/Http/Controllers/Api/v1/UsersController.php
+++ app/Http/Controllers/Api/v1/UsersController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers\Api\v1;
 
-use App\Http\Controllers\Controller;
 
-class UsersController extends Controller
+use App\Http\Filters\v1\UserFilter;
+use App\Http\Resources\v1\UserResource;
+use App\Models\User;
+
+class UsersController extends ApiController
 {
-    //
+    public function index(UserFilter $filters)
+    {
+        return UserResource::collection(User::filter($filters)->paginate());
+    }
+
+    public function show(User $user)
+    {
+        if ($this->include('posts'))
+        {
+           return new UserResource($user->load('posts'));
+        }
+
+        return new UserResource($user);
+    }
 }
