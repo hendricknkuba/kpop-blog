a74573cebb8e4caa50c7f6143ae44261c3dba3c9
leehendrick <imhendrick16@gmail.com>
2025-03-11 00:19:48 -0400

    chore: update user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,7 @@ class UserFactory extends Factory
             'username' => fake()->unique()->username(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'role' => fake()->randomElement(['admin', 'user']),
             'remember_token' => Str::random(10),
         ];
     }
