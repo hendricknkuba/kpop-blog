362ed9ab86bdd16105f09c11550ef818fc9eee75
leehendrick <imhendrick16@gmail.com>
2025-03-11 14:02:46 -0400

    feat: add update & replace & destroy methods in user controller

--- app/Http/Controllers/Api/v1/UsersController.php
+++ app/Http/Controllers/Api/v1/UsersController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\Api\v1;
 
 
 use App\Http\Filters\v1\UserFilter;
+use App\Http\Requests\Api\v1\user\ReplaceUserRequest;
+use App\Http\Requests\Api\v1\user\UpdateUserRequest;
 use App\Http\Resources\v1\UserResource;
 use App\Models\User;
+use App\Policies\v1\UserPolicy;
+use Illuminate\Support\Facades\Gate;
 
 class UsersController extends ApiController
 {
@@ -23,4 +27,33 @@ class UsersController extends ApiController
 
         return new UserResource($user);
     }
+
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        if (Gate::allows('update-user', $user)){
+            $user->update($request->mappedAttributes());
+            return new UserResource($user);
+        }
+
+        return $this->notAuthorized('You are not authorized to update user.');
+    }
+
+    public function replace(ReplaceUserRequest $request, User $user)
+    {
+        if (!Gate::allows('replace-user', $user)){
+            return $this->notAuthorized('You are not authorized to replace user.');
+        }
+
+        $user->update($request->mappedAttributes());
+        return new UserResource($user);
+    }
+
+    public function destroy(User $user)
+    {
+        Gate::authorize('delete-user', $user);
+
+        $user->delete();
+        return $this->ok('User deleted.');
+    }
+
 }
