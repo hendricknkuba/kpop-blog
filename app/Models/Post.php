1599fe7e332541e33d89358730c292da4bc6f61c
leehendrick <imhendrick16@gmail.com>
2025-03-14 01:29:19 -0400

    feat(comments): add relationship between comment and posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Str;
 
 
@@ -43,6 +44,11 @@ class Post extends Model
     public function user() : BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
     }
 
     public function scopeFilter(Builder $builder, QueryFilter $filters): Builder
