a41423549d5a3b9acd005f0c355240fdc8e6e86f
leehendrick <imhendrick16@gmail.com>
2025-03-17 00:53:31 -0400

    chore(routes): change route names

--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
-Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('password.reset');
+Route::post('/password/forgot', [AuthController::class, 'forgotPassword']);
+Route::post('/password/reset', [AuthController::class, 'resetPassword'])->name('password.reset');
 
 
 
